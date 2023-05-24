af2191b029bdd40c810595e786c9d0cd4a0f0793
rifkyhilman <rifkyhilman265@gmail.com>
2023-05-24 09:50:24 +0700

    :memo: Change karma configuration in karma.conf.js
    Change karma configuration in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,18 @@
 module.exports = function (config) {
   config.set({
     /* Kode lainnya disembunyikan */
- 
+
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
- 
+
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
- 
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
- 
+
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity,
